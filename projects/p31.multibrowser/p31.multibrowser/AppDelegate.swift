343e2b353569b9fe0e157403b63acbaece69a50d
Matt Brown <matthew.brown@keysight.com>
2022-08-19 16:49:50 -0400

    minor updates

--- projects/p31.multibrowser/p31.multibrowser/AppDelegate.swift
+++ projects/p31.multibrowser/p31.multibrowser/AppDelegate.swift
@@ -12,6 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 15.0, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.backgroundColor = .systemGray
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
+    
         return true
     }
 
